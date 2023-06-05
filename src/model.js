96fd5937ea2c8f2fecb3b013711878bdd1d1dd7e
Adithya <jhadith88@gmail.com>
2023-06-05 14:58:14 +0530

    done the particles part

--- src/model.js
+++ src/model.js
@@ -2,6 +2,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import * as THREE from 'three';
 
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler';
+
 class Model {
   constructor(obj) {
     console.log(obj);
@@ -22,12 +24,48 @@ class Model {
   init() {
     this.loader.load(this.file, (response) => {
       this.mesh = response.scene.children[0];
+
       this.material = new THREE.MeshBasicMaterial({
         color: 'yellow',
         wireframe: true,
       });
 
       this.mesh.material = this.material;
+
+      this.geometry = this.mesh.geometry;
+
+      // create a particles Materials
+      this.particlesMaterial = new THREE.PointsMaterial({
+        color: 'red',
+        size: 0.02,
+      });
+
+      // create particles geometry
+      const sampler = new MeshSurfaceSampler(this.mesh).build();
+      const numParticles = 20000;
+
+      this.particlesGeometry = new THREE.BufferGeometry();
+      const particlesPosition = new Float32Array(numParticles * 3);
+
+      for (let i = 0; i < numParticles; i++) {
+        const newPosition = new THREE.Vector3();
+        sampler.sample(newPosition);
+        particlesPosition.set(
+          [newPosition.x, newPosition.y, newPosition.z],
+          i * 3
+        );
+      }
+
+      this.particlesGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(particlesPosition, 3)
+      );
+
+      // create the particle
+      this.particles = new THREE.Points(
+        this.particlesGeometry,
+        this.particlesMaterial
+      );
 
       if (this.placeOnLoad) {
         this.add();
@@ -36,11 +74,11 @@ class Model {
   }
 
   add() {
-    this.scene.add(this.mesh);
+    this.scene.add(this.particles);
   }
 
   remove() {
-    this.scene.remove(this.mesh);
+    this.scene.remove(this.particles);
   }
 }
 
